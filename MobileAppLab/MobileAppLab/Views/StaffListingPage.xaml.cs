80ef1378148b77ab25155c1aa13b40ad1b17c35a
annv3 <annv3@TS0813423-PM-I5>
2022-08-29 11:41:02 +0700

    change swipe view from multiple to singe row

--- MobileAppLab/MobileAppLab/Views/StaffListingPage.xaml.cs
+++ MobileAppLab/MobileAppLab/Views/StaffListingPage.xaml.cs
@@ -12,9 +12,26 @@ namespace MobileAppLab.Views
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class StaffListingPage : ContentPage
     {
+        private List<SwipeView> _swipeViews { set; get; } = new List<SwipeView>();
         public StaffListingPage()
         {
             InitializeComponent();
+        }
+        private void SwipeView_SwipeStarted(object sender, SwipeStartedEventArgs e)
+        {
+            //Console.WriteLine("SwipeView_SwipeStarted");
+
+            if (_swipeViews.Count == 1)
+            {
+                _swipeViews[0].Close();
+                _swipeViews.Remove(_swipeViews[0]);
+            }
+        }
+
+        private void SwipeView_SwipeEnded(object sender, SwipeEndedEventArgs e)
+        {
+            //Console.WriteLine("SwipeView_SwipeEnded");
+            _swipeViews.Add((SwipeView)sender);
         }
     }
 }
